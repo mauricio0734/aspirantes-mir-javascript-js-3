6d74173df666e746e42978a84a25f78161cf8d90
Mauricio Barón <diegombaron@gmail.com>
2023-05-19 18:59:01 -0500

    Segundo y tercer ejercicio

--- object1.js
+++ object1.js
@@ -1,5 +1,5 @@
 const Pedro ={
-    
+
     nombre:"Pedro Perez",
     edad: 30,
     activo:  true,
@@ -10,23 +10,24 @@ const Pedro ={
 Pedro['altura'] = 1.8;
 delete Pedro.activo;
 
- function saluda(){
+function saluda(){
 return ' '+ this.nombre;
 }
+
+console.log(Pedro.edad);
+console.log(Pedro);
+console . log ('\n');
+console.log(' Hola me llamo: '+ Pedro.nombre);
 
 //Pedro.
 
 //console.log(Pedro)
- //   console.log(Pedro.edad);
-
 //for(const prop of Pedro.hobbies){
   //  console.log(prop);
  // }
 /*
 Pedro['Descripción'] = function(){
-  return 'Hola mi nombre es ${this.nombre}. tengo ${this.edad} años y mis hobbies son: ${this.hobbies.join(',')}';
+return 'Hola mi nombre es ${this.nombre}. tengo ${this.edad} años y mis hobbies son: ${this.hobbies.
+join(',')}';
 }
 */
-console.log(Pedro);
-console . log ('\n');
-console.log(' Hola me llamo: '+ Pedro.nombre); 
---
+++ object2.js
@@ -0,0 +1,20 @@
+const persona = {
+    nombre: 'Daniel',
+    edad: 30,
+    ciudad: 'Barranquilla',
+    profesion: 'Developed',
+}
+console.log(persona);
+
+function presentacion(person){
+return 'Hola, mi nombre es $(person.nombre}, tengo ${person.edad} años, soy de ${person.ciudad} y soy ${person.profesion}'
+}
+
+const mensaje = presentacion(persona);
+console.log(mensaje);
+persona.hobbies = ['Jugar videojuegos', 'Cocinar', 'Jugar con los perros'];
+console.log(persona.hobbies);
+
+for(const hobbie of persona.hobbies){
+console.log(hobbie);
+}
